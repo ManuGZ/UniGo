917598e1df5062d85e919ae25ddaa8e25ff8395a
ManuGZ <galonso097@gmail.com>
2023-05-27 19:23:24 -0500

    Actualizacion Final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+//let navbar = document.querySelector('.header .navbar');
+
+let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active')
+}
 
 document.onscroll = () => {
     if(window.scrollY > 0){
